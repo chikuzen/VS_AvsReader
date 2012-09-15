c83db6198b41a5477f55f53a171e4814f06209ea
Oka Motofumi <chikuzen.mo@gmail.com>
2012-09-15 19:32:39 +0900

    change license to LGPLv2.1

--- vsavsreader.c
+++ vsavsreader.c
@@ -1,23 +1,23 @@
 /*
-vsavsreader.c: AviSynth Script Reader for VapourSynth
+  vsavsreader.c: AviSynth Script Reader for VapourSynth
 
-Copyright (C) 2012  Oka Motofumi
+  Copyright (C) 2012  Oka Motofumi
 
-Author: Oka Motofumi (chikuzen.mo at gmail dot com)
- 
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
- 
+  Author: Oka Motofumi (chikuzen.mo at gmail dot com)
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
- 
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111, USA.
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with Libav; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 */
 
 
@@ -277,7 +277,7 @@ static VSFormat *set_vs_format(avsr_hnd_t *ah)
     format->subSamplingW = table[i].subsample_w;
     format->subSamplingH = table[i].subsample_h;
     format->numPlanes = avs_is_interleaved(ah->avs_vi) ? 1 : 3;
-    
+
     return format;
 
 }
